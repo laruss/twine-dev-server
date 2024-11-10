480856452a947d60e11bcbeb431b2a1bf206ecd1
konstantin <konstantin@wisebits.com>
2024-11-10 17:42:35 +0200

    remove debug log

--- src/lib/service/noCache.ts
+++ src/lib/service/noCache.ts
@@ -3,7 +3,6 @@ import type { Elysia } from 'elysia';
 export const nocache = (app: Elysia) => {
     // was taken from https://github.com/gaurishhs/elysia-nocache
     return app.onRequest(({ set }) => {
-        console.log('no cache');
         set.headers['Surrogate-Control'] = 'no-store';
         set.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, proxy-revalidate';
         // Deprecated though https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Pragma
